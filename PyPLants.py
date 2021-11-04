52c128e8ea23bcb64c958b8f3a75b0dc3db0c124
Albmargar1 <albertomge@gmail.com>
2021-11-04 15:51:14 +0100

    Test()

--- PyPLants.py
+++ PyPLants.py
@@ -1,4 +1,9 @@
 import os
+import numpy as np
 
-def HelloWorld():
-    print('Hello world!')
+def Test():
+    zero = np.sum(np.zeros(3))
+    if (abs(zero) < 1e-3):
+        print('External modules IS working')
+    else:
+        print('External modules NOT working')
