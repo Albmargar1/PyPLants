f9fba58893b63b848ebe2604a77f6f620e03266c
Albmargar1 <albertomge@gmail.com>
2021-11-04 16:15:06 +0100

    Test matplotlib

--- PyPLants.py
+++ PyPLants.py
@@ -1,10 +1,16 @@
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
-def CheckExternalModules():
-    modules = ['numpy']
+def Check_external_modules():
+    modules = ['numpy', 'matplotlib']
     for m in modules:
         if m not in sys.modules:
             print('ERROR: You have not imported the {} module'.format(m))
             return
     print('All modules imported correctly')
+    
+def Test_plt():
+    x = [1, 2, 3]
+    y = np.array([[1, 2], [3, 4], [5, 6]])
+    plt.plot(x, y)
