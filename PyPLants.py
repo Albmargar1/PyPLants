7b726630dfb82566cdb8af59cdd388004df2d64e
Albmargar1 <albertomge@gmail.com>
2021-11-05 11:18:14 +0100

    Added basic tree L-system
    Credits to: https://github.com/paulgb/notebooks/tree/master/source/l-systems

--- PyPLants.py
+++ PyPLants.py
@@ -1,6 +1,9 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from math import pi, sin, cos
+
+DEGREES_TO_RADIANS = pi / 180
 
 def Check_external_modules():
     modules = ['numpy', 'matplotlib']
@@ -14,3 +17,59 @@ def Test_plt():
     x = [1, 2, 3]
     y = np.array([[1, 2], [3, 4], [5, 6]])
     plt.plot(x, y)
+    
+def Plot_points(points, margins = 0.2, debug_plot = False):
+    
+    if not debug_plot:
+        plt.axis('off')
+        plt.margins(x=margins, y=margins)
+
+    plt.axes().set_aspect('equal', 'datalim')
+    x, y = zip(*points)
+    plt.plot(x, y)
+    
+def L_plot(initiator, generator, iterations = 0, angle = 45):
+    sequence = transform_multiple(initiator, generator, iterations)
+    points = L_system(sequence, turn_angle = angle)
+    Plot_points(points)
+    
+def L_system(sequence, turn_angle=45, start = (0,0), start_angle = 90):
+    saved_states = list()
+    state = (start[0], start[1], start_angle)    
+    yield (start[0], start[1])
+    
+    for instruction in sequence:
+        x, y, angle = state
+        
+        if instruction.lower() in 'abcdef':      # Move turtle forward
+            state = (x - cos(angle * DEGREES_TO_RADIANS),
+                     y + sin(angle * DEGREES_TO_RADIANS),
+                     angle)
+            
+            if instruction.islower():
+                yield (float('nan'), float('nan'))
+                
+            yield (state[0], state[1])
+                        
+        elif instruction == '+':     # Turn turtle clockwise without moving
+            state = (x, y, angle + turn_angle)
+            
+        elif instruction == '-':     # Turn turtle counter-clockwise without moving
+            state = (x, y, angle - turn_angle)
+            
+        elif instruction == '[':                       # Remember current state
+            saved_states.append(state)
+
+        elif instruction == ']':                       # Return to previous state
+            state = saved_states.pop()
+            yield (float('nan'), float('nan'))
+            x, y, _ = state
+            yield (x, y)
+
+def transform_sequence(sequence, transformations):
+    return ''.join(transformations.get(c, c) for c in sequence)
+
+def transform_multiple(sequence, transformations, iterations):
+    for _ in range(iterations):
+        sequence = transform_sequence(sequence, transformations)
+    return sequence
